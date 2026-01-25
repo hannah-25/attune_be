a4c9a1a923601853274a7369e4c81a6d70254174
hannah <judyjjj106@gmail.com>
2026-01-26 03:34:52 +0900

    git init

---
+++ src/main/java/attune/AttuneApplication.java
@@ -0,0 +1,13 @@
+package attune;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttuneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttuneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/attune/AttuneApplicationTests.java
@@ -0,0 +1,13 @@
+package attune;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttuneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
