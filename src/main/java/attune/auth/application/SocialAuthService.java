edde49c8e2957b7b064d12412557d3b5fb86ccbd
hannah <judyjjj106@gmail.com>
2026-06-03 04:07:12 +0900

    fix

--- src/main/java/attune/auth/application/SocialAuthService.java
+++ src/main/java/attune/auth/application/SocialAuthService.java
@@ -43,7 +43,6 @@ public class SocialAuthService {
     private final JwtConfig jwtConfig;
     private final UserAuthCacheRepository userAuthCacheRepository;
     private final ApplicationEventPublisher eventPublisher;
-    private final ApplicationEventPublisher eventPublisher;
 
     public AuthResult login(SocialLoginRequest request) {
         OAuthVerifier verifier = verifiers.stream()
